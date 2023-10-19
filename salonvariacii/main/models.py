5ae7259fc9ff873a94c66f76b73f61f78b5ec58f
MGranc <herrytr@mail.ru>
2023-10-19 15:46:06 +1000

    add kitchen model

--- salonvariacii/main/models.py
+++ salonvariacii/main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Kitchen(models.Model):
+    COLOR_CHOICES = (
+        ('black', "black"),
+        ('white', "white"),
+    )
+
+    STYLE_CHOICES = (
+        ('classic', "classic"),
+        ('modern', "modern"),
+    )
+
+    MATERIAL_CHOICES = (
+        ('PVX', "PVX"),
+        ('LDSP', "LDSP"),
+        ('MDF', "MDF"),
+    )
+
+    FORM_CHOICES = (
+        ('straight', "straight"),
+        ('angular', "angular"),
+        ('narrow', "narrow"),
+    )
+    color = models.CharField('Цвет', choices=COLOR_CHOICES, max_length=50)
+    style = models.CharField('Стиль кухни', choices=STYLE_CHOICES, max_length=50)
+    material = models.CharField('Материал кухни', choices=MATERIAL_CHOICES, max_length=50)
+    form = models.CharField('Форма кухни', choices=FORM_CHOICES, max_length=50)
+    width = models.IntegerField('Ширина кухни')
+
