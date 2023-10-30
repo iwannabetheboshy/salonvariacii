68bbefa6f9d5c4533225556fe6ac264cf1752483
MGranc <herrytr@mail.ru>
2023-10-30 10:15:38 +1000

    add progress fill in mane kitchen slider

--- salonvariacii/main/static/main/js/main-carousel-kitchen.js
+++ salonvariacii/main/static/main/js/main-carousel-kitchen.js
@@ -1,40 +1,35 @@
 $(document).ready(function(){
-    const progressFill = $('.progress-fill');
-    let progressWidth = 0; // Начальная ширина заполняющейся линии
-    const interval = 5000; // Интервал смены слайдов (5 секунд)
+  var $slider = $('.main-kitchen-carousel')
+  var $proggress = $('.progress-fill')
+  var $numbers = $('.num')
+  const interval = 5000;
+  
+  $('.main-kitchen-carousel').on('init', function(event, slick){
+    $numbers.first().addClass('num-active'); 
+    $proggress.first().animate({width: '100%'}, interval, 'linear');
+  });
 
-    function updateProgressBar() {
-        progressWidth = 0; 
-        progressFill.css('width', '0%');
+  $slider.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    prevArrow: $('.custom-prev-arrow'),
+    nextArrow: $('.custom-next-arrow'),
+    autoplay: true,
+    autoplaySpeed: interval,
+    infinite: false, 
+    edgeFriction: 0.2,
 
-        progressFill.animate({
-            width: '100%'
-        }, interval, 'linear');
-    }
+  });
 
-    // Инициализация Slick Slider
-    $('.main-kitchen-carousel').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        variableWidth: true,
-        prevArrow: $('.custom-prev-arrow'),
-        nextArrow: $('.custom-next-arrow'),
-        //autoplay: true,
-        //autoplaySpeed: interval,
-        //onAfterChange: updateProgressBar, 
-        infinite: false, // Отключает бесконечную прокрутку
-        edgeFriction: 0.2, // Добавляет сопротивление на краях слайдера
-        responsive: [
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            
-          ]
-    });
+  $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $numbers.removeClass('num-active');
+    $numbers.eq(nextSlide).addClass('num-active'); 
 
-    //updateProgressBar();
+    var lineToAnimate = $proggress.eq(nextSlide);
+    $proggress.eq(currentSlide).css('width', '0');
+
+    // Анимируем линию (увеличиваем ее ширину до 100%)
+    lineToAnimate.animate({ width: '100%'}, interval, 'linear');;
+  });
 });
