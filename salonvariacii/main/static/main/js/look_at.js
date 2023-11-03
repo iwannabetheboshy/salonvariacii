06a4a10fc2de9954801257e1173007654bdb905a
katya123123 <katyku221133@gmail.com>
2023-11-03 23:39:32 +1000

    like centered and  fix header

--- salonvariacii/main/static/main/js/look_at.js
+++ salonvariacii/main/static/main/js/look_at.js
@@ -1,14 +1,21 @@
-const player = document.getElementById('like');
+$(document).ready(function() {
+    const player = document.getElementByClassName('.like_btn');
 
-player.addEventListener('ready', () => {
-    LottieInteractivity.create({
-        player: player.getLottie(),
-        mode:"cursor",
-        actions: [
-            {
-                type: "click",
-                forceFlag: false
-            }
-        ]
+    player.addEventListener('ready', () => {
+        LottieInteractivity.create({
+            player: player.getLottie(),
+            mode:"cursor",
+            actions: [
+                {
+                    type: "click",
+                    forceFlag: false
+                }
+            ]
+        });
     });
-});
+
+    $('.like_btn').on("click", function(){
+        $('.like').toggle();
+    });
+
+})
