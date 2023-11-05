d1eee3f1ca5335a9673ca24ba7b3bd0e59e57297
iwannabetheboshy <vals.tarasov@gmail.com>
2023-11-05 16:35:37 +1000

    header UI-KIT fix

--- salonvariacii/main/static/main/js/header.js
+++ salonvariacii/main/static/main/js/header.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
         if (navbar.classList.contains('navbar-opened')) {
             navbar.classList.remove('navbar-opened');
             document.getElementById("header").style.background = "rgba(255, 255, 255, .9)"
+            /*document.getElementById("header").style.height = "auto"*/
         }
         else {
             navbar.classList.add('navbar-opened');
             document.getElementById("header").style.background = "rgba(255, 255, 255, 1)"
+            /*document.getElementById("header").style.height = "100%"*/
         }
     });
 });
